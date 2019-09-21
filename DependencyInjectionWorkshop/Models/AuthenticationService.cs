1f8c005efec2265a9366c8c579d3950982cdd3e5
魏佑嘉 <h42279@hotmail.com>
2019-09-21 09:46:15 +0800

    init code (First code form copy)

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,57 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string userAccount, string psw, string otp)
+        {
+            #region Get Psw
+
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                var password = connection.Query<string>("spGetUserPassword", new { Id = userAccount },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            #endregion Get Psw
+
+            #region Get hash
+
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(psw));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            string hashedPsw = hash.ToString();
+
+            #endregion Get hash
+
+            #region Get Otp
+
+            string otpResult;
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", userAccount).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                otpResult = response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{userAccount}");
+            }
+
+            #endregion
+
+            return false;
+        }
     }
 }
