5a1e9be336ab445d7802d4e75517ec777d542fe2
魏佑嘉 <h42279@hotmail.com>
2019-09-21 16:39:11 +0800

    建立基本測試

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -1,4 +1,11 @@
 ﻿using DependencyInjectionWorkshop.Models;
+using DependencyInjectionWorkshop.Models.FailedCounter;
+using DependencyInjectionWorkshop.Models.Hash;
+using DependencyInjectionWorkshop.Models.LogService;
+using DependencyInjectionWorkshop.Models.Notify;
+using DependencyInjectionWorkshop.Models.Otp;
+using DependencyInjectionWorkshop.Models.Profile;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace DependencyInjectionWorkshopTests
@@ -9,7 +16,23 @@ namespace DependencyInjectionWorkshopTests
         [Test]
         public void is_valid()
         {
-            var authenticationService = new AuthenticationService();
+            var profile = Substitute.For<IProfile>();
+            var hash = Substitute.For<IHash>();
+            var otpService = Substitute.For<IOtpService>();
+            var failedCouter = Substitute.For<IFailedCounter>();
+            var logger = Substitute.For<ILogger>();
+            var notification = Substitute.For<INotify>();
+
+            var authenticationService =
+                new AuthenticationService(profile, hash, otpService, notification, logger, failedCouter);
+
+            profile.GetPassword("joey").Returns("my hashed password");
+            hash.Hash("abc").Returns("my hashed password");
+            otpService.GetCurrentOtp("joey").Returns("123456");
+
+            var isValid = authenticationService.Verify("joey", "abc", "123456");
+
+            Assert.IsTrue(isValid);
         }
     }
 }
